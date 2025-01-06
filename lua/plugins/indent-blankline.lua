73bf4c9eeea9ce2011a754d54d7d6dec88aae451
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-01-06 19:56:02 +0000

    Fixed telescope interaction with my custom dynamic highlight Added CommandLineEnter event trigger to handle command line change properly

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -23,11 +23,12 @@ return {
 		}
 
 		-- Forcing indentBlanLine to refresh since it's refresh func doesn't work
-		vim.api.nvim_create_autocmd("ModeChanged", {
+		vim.api.nvim_create_autocmd({ "ModeChanged", "CmdLineEnter" }, {
 			callback = function()
 				-- Check if the current buffer is a Telescope picker
+				-- Telescope piicker's bufferlen is zero for some reason its an empty string
 				local bufname = vim.api.nvim_buf_get_name(0)
-				if bufname:match("") then
+				if string.len(bufname) == 0 then
 					return
 				end
 				-- Otherwise, execute the setup

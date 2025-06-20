c7624f6200e2971d6359c0d292bfbe00f28b60b4
Bryan Van paemel <bryan.vanpaemel@gmail.com>
2025-06-20 14:02:34 +0200

    Add(init.lua)
    - enables inccommand 'split', shows a preview of substitute command
    matches.
      - disabled swap files

--- init.lua
+++ init.lua
@@ -5,40 +5,43 @@ local g   = vim.g
 g.toggle_theme_icon = "   "
 
 -------------------------------------- options ------------------------------------------
-opt.laststatus      = 3 -- global statusline
-opt.showmode        = false
+opt.laststatus     = 3 -- global statusline
+opt.showmode       = false
 
-opt.clipboard       = "unnamedplus"
+opt.inccommand     = 'split'
+opt.swapfile       = false
+
+opt.clipboard      = "unnamedplus"
 
 -- Indenting
-opt.expandtab       = false
-opt.shiftwidth      = 4
-opt.smartindent     = true
-opt.tabstop         = 4
-opt.softtabstop     = 4
+opt.expandtab      = false
+opt.shiftwidth     = 4
+opt.smartindent    = true
+opt.tabstop        = 4
+opt.softtabstop    = 4
 
-opt.ignorecase      = true
-opt.smartcase       = true
-opt.mouse           = "a"
+opt.ignorecase     = true
+opt.smartcase      = true
+opt.mouse          = "a"
 
 -- Numbers
-opt.number          = true
-opt.relativenumber  = true
-opt.numberwidth     = 2
-opt.ruler           = false
+opt.number         = true
+opt.relativenumber = true
+opt.numberwidth    = 2
+opt.ruler          = false
 
 -- disable nvim intro
 opt.shortmess:append "sI"
 
-opt.signcolumn      = "yes"
-opt.splitbelow      = true
-opt.splitright      = true
-opt.termguicolors   = true
-opt.timeoutlen      = 400
-opt.undofile        = true
+opt.signcolumn     = "yes"
+opt.splitbelow     = true
+opt.splitright     = true
+opt.termguicolors  = true
+opt.timeoutlen     = 400
+opt.undofile       = true
 
 -- interval for writing swap file to disk, also used by gitsigns
-opt.updatetime      = 250
+opt.updatetime     = 250
 
 -- when cursor reaches end/beginning of line
 opt.whichwrap:append "<>[]hl"
